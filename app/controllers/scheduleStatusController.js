f5183d32708b984a40a87fabb81a51daa4bf47d6
Pankesh Kumar <pankesh.kumar@vlinkinfo.com>
2017-05-09 15:32:57 +0530

    Scheduler page

--- app/controllers/scheduleStatusController.js
+++ app/controllers/scheduleStatusController.js
@@ -15,26 +15,30 @@
             campaignName: "campaignName 1",
             campaignDescription: "campaignDescription 1",
             campaignType: "SMS",
-            scheduleDateTime: "10/05/2017"
+            scheduleDateTime: "10/05/2017",
+            runStatus:"Scheduled"
         },
-            {
-                campaignName: "campaignName 2",
-                campaignDescription: "campaignDescription 2",
-                campaignType: "EMAIL",
-                scheduleDateTime: "10/05/2017"
-            },
-            {
-                campaignName: "campaignName 3",
-                campaignDescription: "campaignDescription 3",
-                campaignType: "EMAIL & SMS",
-                scheduleDateTime: "10/05/2017"
-            },
-            {
-                campaignName: "campaignName 4",
-                campaignDescription: "campaignDescription 4",
-                campaignType: "EMAIL",
-                scheduleDateTime: "10/05/2017"
-            }];
+        {
+            campaignName: "campaignName 2",
+            campaignDescription: "campaignDescription 2",
+            campaignType: "EMAIL",
+            scheduleDateTime: "08/05/2017",
+            runStatus:"Completed"
+        },
+        {
+            campaignName: "campaignName 3",
+            campaignDescription: "campaignDescription 3",
+            campaignType: "EMAIL & SMS",
+            scheduleDateTime: "10/05/2017",
+            runStatus:"Scheduled"
+        },
+        {
+            campaignName: "campaignName 4",
+            campaignDescription: "campaignDescription 4",
+            campaignType: "EMAIL",
+            scheduleDateTime: "10/05/2017",
+            runStatus:"Pending"
+        }];
 
     };
 
