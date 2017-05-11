ded70ad70fe46be7038b550f63d701818f21d122
nimit <nimit.jain.104@gmail.com>
2017-05-11 14:03:28 +0530

    adding functionality

--- app/controllers/viewCampaign.js
+++ app/controllers/viewCampaign.js
@@ -9,10 +9,11 @@
         }
         init();
 
+		$scope.findData='';
         $scope.campaignList=[];
 		$scope.search_reset=false;
 		$scope.submitData = function(){
-			$scope.search_reset = true;
+			$scope.search_reset = !$scope.search_reset;
 		};
 		
         $scope.campaignList = [{
@@ -41,6 +42,26 @@
 		}];
 
     };
+/*
+function myFunction() {
+  var input, filter, table, tr, td, i;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("myTable");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }       
+  }
+}*/
+
+
 
     viewCampaignController.$inject = ['$scope','$http'];
 
